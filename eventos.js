4fca06538ec86ba37feb1e899fc59114806b3a6e
Jose Riera <joseriveradl@gmail.com>
2018-12-29 23:02:27 -0600

    Dibujo en canvas

--- eventos.js
+++ eventos.js
@@ -1,35 +1,58 @@
-var teclas = {
-  UP: 38,
-  DOWN: 40,
-  RIGTH: 39,
-  LEFTH: 37
-};
-document.addEventListener("keydown", dibujarTeclado);
-var cuadrito = document.getElementById("area_de_dibujo");
-var lienzo = cuadrito.getContext("2d");
-var x = 150;
-var y = 150;
+var estado = 0;          // estado del click
+var colorLinea = "blue";    // color a la linea
+var gLinea = document.getElementById("grosor_linea");
+var button = document.getElementById("bAceptar");
+var cLinea = document.getElementById("color_linea");
+var bColor = document.getElementById("bColor");
 
-dibujarLinea("red", 149,149,151,151);
-function dibujarLinea(color, xInicial, yinicial, xFinal, yFinal)
-{
-  lienzo.beginPath();
-  lienzo.strokeStyle = color;
-  lienzo.lineWidth = 2;
-  lienzo.moveTo(xInicial, yinicial);
-  lienzo.lineTo(xFinal, yFinal);
-  lienzo.stroke();
-  lienzo.closePath();
-  }
-function dibujarTeclado(evento)
-{
-  var colorcito = "blue"
-  var movimiento = 1;
-  switch (evento.keyCode) {
-    case teclas.UP: dibujarLinea(colorcito, x, y, x, y-movimiento); y= y-movimiento; break;
-    case teclas.DOWN: dibujarLinea(colorcito, x, y, x, y+movimiento); y= y+movimiento; break;
-    case teclas.RIGTH: dibujarLinea(colorcito, x + movimiento, y, x, y); x= x+movimiento; break;
-    case teclas.LEFTH: dibujarLinea(colorcito, x-movimiento, y, x, y); x= x-movimiento; break;
-  }
+button.addEventListener("click",dibujarLinea);
+bColor.addEventListener("click",dibujarLinea);
 
+var area = document.getElementById("area_de_dibujo");
+var papel = area.getContext("2d");
+var x;                      // guardar coordenada en X
+var y;                      // guardar coordenada en Y
+document.addEventListener("mousedown",presionarMouse);  //cuando presionas click
+document.addEventListener("mouseup",soltarMouse);       //cuando sueltas click
+document.addEventListener("mousemove",dibujarMouse);    //cuando mueves el mouse
+// dibujo del borde
+var colorBorde = "black"
+var ancho = area.width - 1;
+var alto = area.height - 1;
+dibujarLinea(colorBorde, 1, 1, ancho, 1, papel)
+dibujarLinea(colorBorde, ancho, 1, ancho, alto, papel)
+dibujarLinea(colorBorde, 1, alto,  ancho, alto, papel)
+dibujarLinea(colorBorde, 1, 1, 1, alto, papel)
+// Funcion para mousemove
+function dibujarMouse(evento){
+  if (estado == 1)
+  {   // solo se dibujara si esta el click del mouse presionado
+    dibujarLinea(colorLinea, x, y, evento.layerX, evento.layerY, papel);
+  }
+  x = evento.layerX;
+  y = evento.layerY;
+}
+// Funcion para mousedown
+function presionarMouse(evento){
+  estado = 1;         //click presionado
+  x = evento.layerX;
+  y = evento.layerY;
+}
+// Funcion para mouseup
+function soltarMouse(evento){
+  estado = 0;         // click suelto
+  x = evento.layerX;
+  y = evento.layerY;
+}
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
+{
+  var numero = parseInt(gLinea.value);
+  var color = cLinea.value;
+  lienzo.beginPath();                  // Inicia el trazo
+  lienzo.strokeStyle = color;          // Estilo de trazo (color)
+  lienzo.lineWidth = numero;                // Ancho de la linea
+  lienzo.moveTo(xinicial, yinicial);   // Donde comienza la linea
+  lienzo.lineTo(xfinal, yfinal);       // Traza la linea (ubica punto final)
+  lienzo.stroke();                     // Dibuja con el estio de trazo
+  lienzo.closePath();                  // Cierra el dibujo
 }
